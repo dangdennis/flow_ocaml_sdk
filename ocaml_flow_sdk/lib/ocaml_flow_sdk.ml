53882bc26b7539872fdea7980701c444b9ed21f2
Dennis Dang <dangggdennis@gmail.com>
2022-02-26 15:25:42 -0500

    base64 encode request script

--- ocaml_flow_sdk/lib/ocaml_flow_sdk.ml
+++ ocaml_flow_sdk/lib/ocaml_flow_sdk.ml
@@ -6,35 +6,58 @@ type t = { network_addr : string }
 module Network = struct
   type t = Testnet | Mainnet
 
-  let of_string network =
+  let to_string network =
     match network with
     | Testnet -> "https://rest-testnet.onflow.org/v1/"
     | Mainnet -> "https://rest-mainnet.onflow.org/v1/"
 end
 
+module Endpoint = struct
+  type t = Scripts
+
+  let to_url network endpoint =
+    let net = Network.to_string network in
+    match endpoint with Scripts -> net ^ "scripts"
+end
+
 let make addr = { network_addr = addr }
 
-let send_script network _script =
+let send_script network script =
   let p =
-    let net_url = Network.of_string network in
-    let body =
-      Cohttp_lwt.Body.of_string
-        {|
-      {
-        "script": "cHViIGZ1biBtYWluKCk6IEludCB7IHJldHVybiAxIH0=",
-        "arguments": []
-    }
-    |}
-    in
-    let headers =
-      Cohttp.Header.of_list [ ("Content-Type", "application/json") ]
-    in
-    let* _res, res_body =
-      Client.post ~body ~headers (Uri.of_string (net_url ^ "/scripts"))
-    in
-    let* str = Cohttp_lwt.Body.to_string res_body in
-    print_endline str;
-    Lwt.return_unit
+    match Base64.encode script with
+    | Ok res ->
+        let body =
+          res
+          |> Format.sprintf
+               {|
+                  {
+                    "script": "%s",
+                    "arguments": []
+                  }
+                |}
+          |> Cohttp_lwt.Body.of_string
+        in
+
+        let headers =
+          Cohttp.Header.of_list [ ("Content-Type", "application/json") ]
+        in
+
+        let* res, res_body =
+          Client.post ~body ~headers
+            (Uri.of_string (Endpoint.to_url network Endpoint.Scripts))
+        in
+
+        let status_code = Cohttp.Response.status res in
+
+        let* res_body_str = Cohttp_lwt.Body.to_string res_body in
+
+        Printf.printf "statusCode %s" (Cohttp.Code.string_of_status status_code);
+        print_endline "start resp";
+        print_endline res_body_str;
+        print_endline "end resp";
+
+        Lwt.return_unit
+    | Error _ -> Lwt.return_unit
   in
   let _ = Lwt_main.run p in
   ()
--- ocaml_flow_sdk/test/test_ocaml_flow_sdk.ml
+++ ocaml_flow_sdk/test/test_ocaml_flow_sdk.ml
@@ -3,5 +3,5 @@ module F = Ocaml_flow_sdk
 
 let () =
   print_endline "hi";
-  let _ = F.send_script F.Network.Testnet "" in
+  let _ = F.send_script F.Network.Testnet "pub fun main(): Int { return 1 }" in
   ()
